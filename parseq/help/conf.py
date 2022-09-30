40fa83ecdb9883fc6c6036bb5613acfbdd6c02c8
Konstantin Klementiev <konstantin.klementiev@gmail.com>
2022-09-30 19:27:02 +0200

    Sphinx conf change

--- parseq/help/conf.py
+++ parseq/help/conf.py
@@ -20,6 +20,7 @@ if on_rtd:
     MOCK_MODULES = ['pyopencl',
                     'PyQt5', 'PyQt5.QtCore', 'PyQt5.QtGui', 'PyQt5.QtWidgets',
                     'PyQt5.QtWebEngineWidgets',
+                    'silx.gui'
                     ]
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
