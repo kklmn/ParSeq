728525d7027e2579cb3a56f37b4b28cbae3d5bb5
Konstantin Klementiev <konstantin.klementiev@gmail.com>
2023-03-09 14:21:15 +0100

    v 0.9.92

--- parseq/version.py
+++ parseq/version.py
@@ -1,4 +1,4 @@
 ﻿# -*- coding: utf-8 -*-
-__versioninfo__ = (0, 9, 91)
+__versioninfo__ = (0, 9, 92)
 __version__ = '.'.join(map(str, __versioninfo__))
-__date__ = "7 Mar 2023"
+__date__ = "9 Mar 2023"
