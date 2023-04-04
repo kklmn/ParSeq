cd4c05fe37525b1c437153ac4bca3856d6fabe88
Konstantin Klementiev <konstantin.klementiev@gmail.com>
2023-04-04 11:38:32 +0200

    remove dependency on XAFSmass

--- parseq/third_party/XAFSmass.py
+++ parseq/third_party/XAFSmass.py
@@ -6,15 +6,17 @@ __date__ = "13 Feb 2022"
 # path to xrt:
 import sys; sys.path.append(r'c:\Ray-tracing\XAFSmass')  # analysis:ignore
 import os
-import XAFSmass as xm
+# import XAFSmass as xm
 # import XAFSmass.XAFSmassQt as xmq
 
 edges = ("K", "L1", "L2", "L3", "M1", "M2", "M3", "M4", "M5", "N1", "N2", "N3")
 
+# selfDir = os.path.dirname(xm.__file__)
+selfDir = os.path.dirname(__file__)
+
 
 def read_energies():
     """Read `Energies.txt` and return a list of 'Z Element edge energy'."""
-    selfDir = os.path.dirname(xm.__file__)
     efname = os.path.join(selfDir, 'data', 'Energies.txt')
     energies = []
     with open(efname, 'r') as f:
--- parseq/version.py
+++ parseq/version.py
@@ -1,4 +1,4 @@
 ﻿# -*- coding: utf-8 -*-
-__versioninfo__ = (0, 9, 96)
+__versioninfo__ = (1, 0, 0)
 __version__ = '.'.join(map(str, __versioninfo__))
-__date__ = "28 Mar 2023"
+__date__ = "4 Apr 2023"
