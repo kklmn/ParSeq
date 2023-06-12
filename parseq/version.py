fc21358a9c686dcf6cba88106fa89653a2ae1e38
Konstantin Klementiev <konstantin.klementiev@gmail.com>
2023-06-12 23:11:44 +0200

    Update version.py

--- parseq/version.py
+++ parseq/version.py
@@ -1,4 +1,4 @@
 ﻿# -*- coding: utf-8 -*-
-__versioninfo__ = (1, 0, 0)
+__versioninfo__ = (2023, 6, 0)
 __version__ = '.'.join(map(str, __versioninfo__))
-__date__ = "4 Apr 2023"
+__date__ = "13 Jun 2023"
