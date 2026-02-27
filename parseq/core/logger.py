8ba6c6135f7e3df93b5b51f56780ef0704796a40
Konstantin Klementiev <konstantin.klementiev@gmail.com>
2026-02-27 08:53:58 +0100

    add cumulative logging to time profiling

--- parseq/core/logger.py
+++ parseq/core/logger.py
@@ -12,7 +12,9 @@ try:
     import colorama
     colorama.init(autoreset=True)
     green = colorama.Fore.GREEN
+    cyan = colorama.Fore.CYAN
     red = colorama.Fore.RED
+    yellow = colorama.Fore.YELLOW
     reset = colorama.Fore.RESET
 except ImportError:
     colorama = None
@@ -32,36 +34,60 @@ console = logging.StreamHandler()
 console.setLevel(logging.INFO)
 syslogger.addHandler(console)
 
-longExecutionTime = 0.5  # s
+timeLevels = 0.1, 0.5  # in s, borders that separate green, cyan and red
 
 
 def logger(minLevel=1, printClass=False, attrs=None):
     """Printed if csi.DEBUG_LEVEL > *minLevel*.
-       *attrs*: a list of (iarg[int], attr[str]) pairs to print out.
+
+    *attrs*: a list of (iarg[int], attr[str]) pairs to print out, where
+             iarg refers to the position in the *args* list of the function.
     """
     def decorate(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             if csi.DEBUG_LEVEL > minLevel:
                 out = f"{func.__name__}()"
+                timesKey = str(out)
                 if printClass:
                     out += f" of {args[0].__class__.__name__}"
                 if attrs:
                     for iarg, attr in attrs:
                         try:
-                            out += f", {attr}={getattr(args[iarg], attr)}"
+                            dout = f", {attr}={getattr(args[iarg], attr)}"
+                            out += dout
+                            if attr != 'alias':
+                                timesKey += dout
                         except Exception as e:
                             out += f", {e}"
                 syslogger.log(csi.DEBUG_LEVEL, f"enter {out}")
                 tstart = time.time()
+                lastAction = "replot(), node='{0}'".format(
+                    list(csi.nodes.keys())[-1])
             res = func(*args, **kwargs)
             if csi.DEBUG_LEVEL > minLevel:
                 dt = time.time() - tstart
-                mark = green if dt < longExecutionTime else red
-                syslogger.log(csi.DEBUG_LEVEL,
-                              f"{out} has taken {mark}{dt:.6f}{reset}s")
+                if timesKey not in csi.exectimes:
+                    csi.exectimes[timesKey] = dt
+                else:
+                    csi.exectimes[timesKey] += dt
+                mark = green if dt < timeLevels[0] else \
+                    cyan if dt < timeLevels[1] else red
+                syslogger.log(
+                    csi.DEBUG_LEVEL, f"{out} has taken {mark}{dt:.6f}{reset}s")
                 if 'worker' in out:
                     syslogger.log(csi.DEBUG_LEVEL, "")
+
+                if out == lastAction and (csi.DEBUG_LEVEL > minLevel):
+                    syslogger.log(csi.DEBUG_LEVEL, "")
+                    for tkey, dt in dict(csi.exectimes).items():
+                        mark = green if dt < timeLevels[0] else \
+                            cyan if dt < timeLevels[1] else red
+                        syslogger.log(
+                            csi.DEBUG_LEVEL, f"{yellow}cumulative{reset} "
+                            f"{tkey} took {mark}{dt:.6f}{reset}s")
+                    csi.exectimes = dict()
             return res
         return wrapper
+
     return decorate
