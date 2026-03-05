fb027d50f032f394d69652905116310424e24395
Konstantin Klementiev <konstantin.klementiev@gmail.com>
2026-03-05 11:40:13 +0100

    prepare release 2026.3.0

--- parseq/version.py
+++ parseq/version.py
@@ -1,4 +1,4 @@
 ﻿# -*- coding: utf-8 -*-
-__versioninfo__ = (2026, 1, '0')
+__versioninfo__ = (2026, 3, '0')
 __version__ = '.'.join(map(str, __versioninfo__))
-__date__ = "5 Jan 2026"
+__date__ = "5 Mar 2026"
